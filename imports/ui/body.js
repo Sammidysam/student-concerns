223fdc615dad74a39e6ded110bcaf1b2954a65fd
Sammidysam <sammidysam@gmail.com>
2016-12-30 14:56:33 -0500

    Make the form do stuff
    Still need to hook accounts up to the anonymous flag

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,3 +1,22 @@
 import { Template } from 'meteor/templating';
 
+import { Concerns } from '../api/concerns.js';
+
 import './body.html';
+
+Template.body.events({
+	'submit .new-concern'(event) {
+		event.preventDefault();
+
+		const target = event.target;
+		const anonymous = target.anonymous.checked;
+		const text = target.text.value;
+
+		Concerns.insert({
+			text,
+			createdAt: new Date()
+		});
+
+		target.text.value = '';
+	}
+});
